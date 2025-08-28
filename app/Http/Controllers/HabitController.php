6faf5b637f5d3a199e0236652ebc7e873db404a4
Srmess <boynerd@outlook.com>
2025-08-28 15:28:23 -0300

    [FEAT]: Creating delete habit function and adding resource to habits route

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -10,9 +10,6 @@ use Illuminate\Http\Request;
 
 class HabitController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $habits = Habit::all();
@@ -20,9 +17,6 @@ class HabitController extends Controller
         return HabitResource::collection($habits);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreHabitRequest $request)
     {
         $payload = $request->only(['title', 'uuid']);
@@ -32,17 +26,11 @@ class HabitController extends Controller
         return HabitResource::make($habit);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Habit $habit)
     {
         return HabitResource::make($habit);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateHabitRequest $request, Habit $habit)
     {
         $payload = $request->only(['title', 'uuid']);
@@ -52,11 +40,10 @@ class HabitController extends Controller
         return HabitResource::make($habit);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Habit $habit)
     {
-        //
+        $habit->delete();
+
+        return response()->noContent();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn() => config('app.name'));
 
 Route::prefix('/api')->name('api.')->group(function () {
-    Route::get('/habits', [HabitController::class, 'index'])->name('habits.index');
-    Route::get('/habits/{habit:uuid}', [HabitController::class, 'show'])->name('habits.show');
-
-    Route::post('/habits', [HabitController::class, 'store'])->name('habits.store');
-
-    Route::put('/habits/{habit:uuid}', [HabitController::class, 'update'])->name('habits.update');
+    Route::resource('habits', HabitController::class)->scoped(['habit' => 'uuid']);
 });
